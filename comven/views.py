3f89ef76bc9e80a71a9f2af087659ad154e6d99c
pirriadoc <butcher198038@gmail.com>
2015-10-15 21:59:32 +0200

    templates + urls

--- comven/views.py
+++ comven/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from .models import Juego
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Indice del Olimpo del Gamer")
+    juego = Juego.objects.order_by('-titulo')[:5]
+    context = {'juego': juego}
+    return render(request, 'comven/index.html', context)
+def detail(request, Juego_id):
+    juego = get_object_or_404(Juego, pk=Juego_id)
+    return render(request, 'comven/detail.html', {'juego': juego})
