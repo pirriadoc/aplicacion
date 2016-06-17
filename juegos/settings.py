e6dc7911759bd1b340aeca0597f782e14a568014
Javier Garcia Pajuelo <pirriadoc@users.noreply.github.com>
2016-06-17 19:50:38 +0200

    Update settings.py

--- juegos/settings.py
+++ juegos/settings.py
@@ -83,8 +83,11 @@ WSGI_APPLICATION = 'juegos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.posgresql',
+        'NAME': 'posgresql',
+        'USER': 'javier'
+        'PASSWORD': 'javier'
+        'HOST': ''
     }
 }
 
